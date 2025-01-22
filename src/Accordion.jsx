a54ff11044f191cda0bf7ed13ad6ce49d6af063a
Raramuri <raramuri@Raramuris-MacBook-Pro.local>
2025-01-22 18:21:21 +0530

    completed accordion assignment

---
+++ src/Accordion.jsx
@@ -0,0 +1,25 @@
+import React, { useRef, useState } from "react";
+import "./App.css";
+
+function Test(props) {
+  const [show, setShow] = useState(false);
+
+  const handledropdown = () => {
+    setShow((prev) => (prev = !prev));
+  };
+
+  return (
+    <div onClick={handledropdown}>
+      {props.title}{" "}
+      <span
+        aria-hidden={true}
+        className={
+          show ? "accordion-icon" : "accordion-icon-rotated accordion-icon"
+        }
+      />
+      {show && <p className="p">{props.content}</p>}
+    </div>
+  );
+}
+
+export default Test;
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
-import Test2 from "./day_11/test2";
-import Counter from "./day_8/count";
-import SignForm from "./day_8/SignForm";
-import DelayCounter from "./usememo /usememo1";
-import Memo from "./usememo /usememo2";
+import Accordion from "./Accordion.jsx";
+// import Test2 from "./day_11/test2";
+// import Counter from "./day_8/count";
+// import SignForm from "./day_8/SignForm";
+// import DelayCounter from "./usememo /usememo1";
+// import Memo from "./usememo /usememo2";
 // import Counter from "./components/Counter";
 
 // import Buttons from "./day_8/Buttons";
@@ -11,14 +12,44 @@ import Memo from "./usememo /usememo2";
 // import Test from "./day_10/Test";
 
 function App() {
-  // return <Buttons />;
-  // return <Counter />;
-  // return <SignForm />;
-  return <Test2 />;
-  // return <DelayCounter />;
-  // return <Memo />;
+  // // return <Buttons />;
+  // // return <Counter />;
+  // // return <SignForm />;
+  // return <Test2 />;
+  // // return <DelayCounter />;
+  // // return <Memo />;
 
-  return;
+  // return;
+  const arr = [
+    {
+      title: "HTML",
+      content: `The HyperText Markup Language or HTML is the
+    standard markup language for documents designed to
+    be displayed in a web browser.`,
+    },
+    {
+      title: "CSS",
+      content: `Cascading Style Sheets is a style sheet language
+    used for describing the presentation of a document
+    written in a markup language such as HTML or XML.`,
+    },
+    {
+      title: "JS",
+      content: `JavaScript, often abbreviated as JS, is a
+    programming language that is one of the core
+    technolog`,
+    },
+  ];
+
+  return (
+    <>
+      <div className="acc-maindiv">
+        {arr.map((ele, key) => (
+          <Accordion title={ele.title} content={ele.content} />
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default App;
