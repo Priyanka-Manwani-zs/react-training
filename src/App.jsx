3a49ce0603824b0145668289d139c4f54156662c
Raramuri <raramuri@Raramuris-MacBook-Pro.local>
2025-01-30 23:53:37 +0530

    merged up until day_13

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 // import "./App.css";
 
-import AccordionDisplay from "./Accordion/AccordionDisplay.jsx";
+// import AccordionDisplay from "./Accordion/AccordionDisplay.jsx";
 
 import "./App.css";
 
-import Test2 from "./day_11/test2";
+// import Test2 from "./day_11/test2";
 
-import Counter from "./day_8/count";
-import SignForm from "./day_8/SignForm";
-import DelayCounter from "./usememo /usememo1";
-import Memo from "./usememo /usememo2";
+// import Counter from "./day_8/count";
+// import SignForm from "./day_8/SignForm";
+// import DelayCounter from "./usememo /usememo1";
+// import Memo from "./usememo /usememo2";
 
 // import Form from "./day_10/Test";
 
@@ -19,44 +19,38 @@ import Memo from "./usememo /usememo2";
 // import DelayCounter from "./usememo /usememo1";
 // import Memo from "./usememo /usememo2";
 
-// import Counter from "./components/Counter";
+import Counter from "./components/Counter";
 import React, { useState } from "react";
 
-import ModalDisplay from "./day_13/ModalDisplay";
+// import ModalDisplay from "./day_13/ModalDisplay";
 
-// import Display from "./components/Display";
+import Display from "./components/Display";
 
 // import Buttons from "./day_8/Buttons";
+// import Button1 from "./day_11/button.jsx";
 // import Text from "./components/Text";
 // import Test from "./day_10/Test";
 
 function App() {
-  // // return <Buttons />;
   // // return <Counter />;
   // // return <SignForm />;
   // return <Test2 />;
   // // return <DelayCounter />;
   // // return <Memo />;
-
   // return;
-
   // return <Buttons />;
   // return <Display />;
-
   // return <Counter />;
   // return <SignForm />;
-
   // return <Test2 />;
   // return <DelayCounter />;
   // return <Memo />;
-  return <ModalDisplay />;
+  // return <ModalDisplay />;
   // return <Modal />;
-
   // return <Test2 />;
   // return <DelayCounter />;
   // return <Memo />;
   // return <Form />;
-
   // return (
   //   <>
   //     <AccordionDisplay />
